46da8e9d5a6e03263e07bf72660d8736dd895e36
Diego Giacomelli <giacomelli@gmail.com>
2013-05-25 11:47:38 -0300

    Prepared to run on mono (tested at MacOSX).

--- Swagger.Net.UnitTests/XmlCommentDocumentationProviderTest.cs
+++ Swagger.Net.UnitTests/XmlCommentDocumentationProviderTest.cs
@@ -2,14 +2,14 @@
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
 namespace Swagger.Net.UnitTests
 {
-	[TestClass]
+	[TestFixture]
 	public class XmlCommentDocumentationProviderTest
 	{
-		[TestMethod]
+		[Test]
 		public void ProcessTypeName_GenericList_SpecList()
 		{
 			var typeName = "System.Collections.Generic.List`1[[Terra.Adv.AdApi.Domain.LineItems.LineItem, Terra.Adv.AdApi.Domain, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null]]";
---
+++ tools/restoreNuGetPackages.sh
@@ -0,0 +1,3 @@
+mono --runtime=v4.0.30319 ../.nuget/nuget.exe install "../Swagger.Net/packages.config" -source "" -solutionDir "" -OutputDirectory "../packages"
+
+mono --runtime=v4.0.30319 ../.nuget/nuget.exe install "../Swagger.Net.WebApi/packages.config" -source "" -solutionDir "" -OutputDirectory "../packages"
